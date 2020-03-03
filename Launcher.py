e4181dc469de649f48c72ef671443349d90c6884
Stefan Kobstädt <kobstaedt.development@gmail.com>
2020-03-03 17:40:22 +0100

    Implemented Launcher class.

--- Launcher.py
+++ Launcher.py
@@ -1,1 +1,24 @@
- 
+import pandas
+
+from api.quandl.helper.DownloaderHelper import DownloaderHelper
+
+
+class Launcher:
+    """
+    """
+
+
+    def __init__(self) -> None:
+        """
+        """
+
+        self.start()
+
+
+    def start(self) -> None:
+        """
+        """
+
+
+if __name__ == '__main__':
+    Launcher()

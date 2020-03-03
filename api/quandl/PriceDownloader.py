44618c39fbc8164f1d33eec339816bf9db87358c
Stefan Kobstädt <kobstaedt.development@gmail.com>
2020-03-03 17:29:44 +0100

    Implemented PriceDownloader class in API package.

---
+++ api/quandl/PriceDownloader.py
@@ -0,0 +1,28 @@
+import pandas
+
+import quandl
+
+from base.IDownloader import IDownloader
+from helper.DownloaderHelper import DownloaderHelper
+
+class PriceDownloader(IDownloader):
+    """
+    """
+
+    def __init__(self, tickerName: str, startDate: str = '2008-01-01', frequency: str = "monthly") -> None:
+        """
+        """        
+        
+        quandl.ApiConfig.api_key = DownloaderHelper.API_AUTHENTICATION_KEY
+
+        self.__tickerName = tickerName
+        self.__startDate = startDate
+        self.__frequency = frequency
+
+
+    # Override
+    def download(self) -> pandas.core.frame.DataFrame:
+        """
+        """
+
+        return quandl.get(self.__tickerName, start_date = self.__startDate, collapse = self.__frequency)

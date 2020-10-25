a5a3e82b98ab8d41297fcd04379f435cf0e5ae53
Stfn Kbst <kobstaedt.development@gmail.com>
2020-10-25 06:28:30 +0000

    Update all.

--- src/__main__.py
+++ src/__main__.py
@@ -2,10 +2,11 @@
 # -*- coding: utf-8 -*-
 
 from client.Client import Client
-
+import client.quickstart as qs
 
 if __name__ == '__main__':
   
-  client: Client = Client()  
-  client.start()
+  #client: Client = Client()  
+  #client.start()
+  qs.main()
   
--- src/client/Client.py
+++ src/client/Client.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+import client.quickstart
 
 from client.AbstractBaseClient import AbstractBaseClient
 from commons.decorators.override import Override
@@ -20,6 +21,31 @@ class Client(AbstractBaseClient):
 
   @Override(AbstractBaseClient)
   def start(self) -> None:
-
-    print ("Test")
     
+    self.quickstart
+
+
+  #@Override(AbstractBaseClient)
+  #def start1(self) -> None:
+
+    # Python code to illustrate Sending mail from  
+    # your Gmail account  
+  #  import smtplib 
+      
+    # creates SMTP session 
+  #  s = smtplib.SMTP('smtp.gmail.com', 587) 
+      
+    # start TLS for security 
+  #  s.starttls() 
+      
+    # Authentication 
+  #  s.login("kobstaedt.finances@gmail.com", "Trilobit#2018") 
+      
+    # message to be sent 
+  #  message = "Message_you_need_to_send"
+      
+    # sending the mail 
+    #s.sendmail("sender_email_id", "receiver_email_id", message) 
+      
+    # terminating the session 
+  #  s.quit() 
